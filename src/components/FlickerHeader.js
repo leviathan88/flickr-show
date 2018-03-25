5e5a4bd075fd7c9b688df255ee255f124cbadf96
Elvis Dedic <elvis26112009@gmail.com>
2018-03-25 15:43:20 +0200

    remove container

--- src/components/FlickerHeader.js
+++ src/components/FlickerHeader.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import ReactNative from 'react-native'
 import { Container, Header, Body, Title } from 'native-base'
 
 const FlickerHeader = () => {
-  return (
-    <Container>
-      <Header>        
-        <Body>
-          <Title>Flicker Show</Title>
-        </Body>        
-      </Header>
-    </Container>
+  return (    
+    <Header>        
+      <Body>
+        <Title>Flicker Show</Title>
+      </Body>        
+    </Header>    
   )
 }
 
