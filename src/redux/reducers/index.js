f45195e7dff9bfa8ad6f51073189f135f3648031
Elvis Dedic <elvis26112009@gmail.com>
2018-03-25 20:47:11 +0200

    clean up

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,5 @@
 import { combineReducers } from 'redux'
-import { combineEpics, createEpicMiddleware } from 'redux-observable'
 import { Flicker } from './flicker'
-
-// const epic = combineEpics(
-
-// )
-
-// export const epicMiddleware = createEpicMiddleware(epicMiddleware)
 
 export const reducers = combineReducers({
   Flicker: Flicker
