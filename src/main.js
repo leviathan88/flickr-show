f06f99868f9612b985c8fa02a507bf976f3babc7
Elvis Dedic <elvis26112009@gmail.com>
2018-03-25 16:30:11 +0200

    add flicker list

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Container, Content } from 'native-base'
 import FlickerHeader from './components/FlickerHeader'
 import FlickerSearch from './components/FlickerSearch'
+import FlickerList from './components/FlickerList'
 
 const MainComponent = () =>
   <Container>
     <FlickerHeader />
     <Content>
       <FlickerSearch />
+      <FlickerList />
     </Content>
   </Container>
 
