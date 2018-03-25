827744df764e90f8d8bd4f48ff83090988fedef4
Elvis Dedic <elvis26112009@gmail.com>
2018-03-25 15:43:47 +0200

    add content with flicker search

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Container } from 'native-base'
+import { Container, Content } from 'native-base'
 import FlickerHeader from './components/FlickerHeader'
+import FlickerSearch from './components/FlickerSearch'
 
 const MainComponent = () =>
   <Container>
     <FlickerHeader />
+    <Content>
+      <FlickerSearch />
+    </Content>
   </Container>
 
 export default MainComponent
