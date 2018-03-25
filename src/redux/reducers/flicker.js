91c150cca03fbdd2662031996a2e8d686233cdc6
Elvis Dedic <elvis26112009@gmail.com>
2018-03-25 18:06:34 +0200

    initial flicker reducer

---
+++ src/redux/reducers/flicker.js
@@ -0,0 +1,193 @@
+export function Flicker(state = initialState, { type, payload }) {
+  switch (type) {    
+  
+    default:
+      return state
+  }
+}
+
+
+
+const examplePhotos = [
+  {
+    "id": "26136544427",
+    "owner": "141988541@N06",
+    "secret": "f591286e98",
+    "server": "4795",
+    "farm": 5,
+    "title": "Horseguards London",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136545647",
+    "owner": "160484342@N02",
+    "secret": "5acdd841f7",
+    "server": "4788",
+    "farm": 5,
+    "title": "Goodbye winter.",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136548867",
+    "owner": "140424278@N07",
+    "secret": "9749f5045d",
+    "server": "817",
+    "farm": 1,
+    "title": "imsi20180325111853",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136554607",
+    "owner": "102398685@N04",
+    "secret": "7fe988d501",
+    "server": "4782",
+    "farm": 5,
+    "title": "Pinned to How To Make It on Pinterest",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136562317",
+    "owner": "42444189@N04",
+    "secret": "9d736b9c6a",
+    "server": "802",
+    "farm": 1,
+    "title": "Former SF 463 032418 Rear",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136564997",
+    "owner": "26601062@N03",
+    "secret": "5764907c37",
+    "server": "790",
+    "farm": 1,
+    "title": "Photo",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136565347",
+    "owner": "137716734@N07",
+    "secret": "9d5beb31ee",
+    "server": "790",
+    "farm": 1,
+    "title": "Garden",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136565887",
+    "owner": "56996876@N06",
+    "secret": "cb4eeaf374",
+    "server": "811",
+    "farm": 1,
+    "title": "20180324_140224",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136567097",
+    "owner": "43256651@N07",
+    "secret": "0a566d03f7",
+    "server": "784",
+    "farm": 1,
+    "title": "DSC_0033.jpg",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136567317",
+    "owner": "60198488@N06",
+    "secret": "ed6cdbb379",
+    "server": "820",
+    "farm": 1,
+    "title": "DSC_8791.jpg",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136568997",
+    "owner": "123953722@N08",
+    "secret": "3a804d6f46",
+    "server": "4781",
+    "farm": 5,
+    "title": "..",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136572957",
+    "owner": "48457902@N04",
+    "secret": "8989c66f7b",
+    "server": "812",
+    "farm": 1,
+    "title": "knitting006",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136574857",
+    "owner": "156223311@N04",
+    "secret": "1fa4a192b1",
+    "server": "4795",
+    "farm": 5,
+    "title": "Buster Olney: What I would bet farm on in 2018 Major League Baseball – Buster Olney Blog",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136576407",
+    "owner": "148656842@N07",
+    "secret": "610f87b3b2",
+    "server": "4795",
+    "farm": 5,
+    "title": "Photo",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136576527",
+    "owner": "106169014@N05",
+    "secret": "414790efaf",
+    "server": "4778",
+    "farm": 5,
+    "title": "DSC_7424.jpg",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+  {
+    "id": "26136576697",
+    "owner": "25793518@N03",
+    "secret": "35b3159988",
+    "server": "4785",
+    "farm": 5,
+    "title": "SPW_0693.jpg",
+    "ispublic": 1,
+    "isfriend": 0,
+    "isfamily": 0
+  },
+]
+
+const initialState = {
+  photos: examplePhotos
+ }
+
