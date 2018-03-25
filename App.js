9e170c9a29419c21751b8a05bce5334b19a6a78f
Elvis Dedic <elvis26112009@gmail.com>
2018-03-25 16:30:02 +0200

    wrap it with provider

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+
 import MainComponent from './src/main'
+import { store } from './src/redux'
 
 export default class App extends Component {
   render() {
     return (
+    <Provider store={store} >
       <MainComponent />
+    </Provider>
     )
   }
 }
