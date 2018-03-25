92e657cd5e462f3162eb9b5681c9b024ac7858a0
Elvis Dedic <elvis26112009@gmail.com>
2018-03-25 14:30:47 +0200

    new stuff

--- App.js
+++ App.js
@@ -1,39 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
+import getTheme from './native-base-theme/components'
+import { StyleProvider } from 'native-base'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <StyleProvider style={getTheme()}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+          Ninjas
+        </Text>        
+      </StyleProvider>
     );
   }
 }
