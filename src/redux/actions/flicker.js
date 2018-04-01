d900d573a9d5b57b8e66a95881148a15f189c6ec
Elvis Dedic <elvis26112009@gmail.com>
2018-04-01 10:45:12 +0200

    more actions

--- src/redux/actions/flicker.js
+++ src/redux/actions/flicker.js
@@ -1,8 +1,14 @@
-import { ON_SEARCH_PHOTOS_ENTER } from "../constants/flicker"
+import { ON_SEARCH_PHOTOS_ENTER, ON_LOAD_MORE_PHOTOS } from "../constants/flicker"
 
 export function onSearchInputEnter(text) {  
   return {
     type: ON_SEARCH_PHOTOS_ENTER,
     payload: text
+  }
+}
+
+export function loadMorePhotos() {
+  return {
+    type: ON_LOAD_MORE_PHOTOS
   }
 }
