f14677f97f87ead86edd9b0083006f608ce1bd04
Elvis Dedic <elvis26112009@gmail.com>
2018-04-01 16:30:43 +0200

    include photo details

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Scene, Router, ActionConst } from 'react-native-router-flux'
 import List from './components/List'
+import PhotoDetails from './components/Details/PhotoDetails'
 
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key='root' hideNavBar>
+      <Scene key='root'>
         <Scene key='list' hideNavBar component={List} />
-        <Scene key='details' component={List} />
+        <Scene key='details' component={PhotoDetails} />
       </Scene>
     </Router>
   )
