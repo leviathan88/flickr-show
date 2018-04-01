3a3cf4f04f28e33eccd6f1f7d0488c5d61095383
Elvis Dedic <elvis26112009@gmail.com>
2018-04-01 16:32:40 +0200

    more photo service

--- src/services/photos.js
+++ src/services/photos.js
@@ -3,13 +3,18 @@ function getPhotoUrl(farmId, serverId, photoId, secret) {
 }
 
 const getFlickerAPI = method => `https://api.flickr.com/services/rest/?method=${method}&api_key=ad24bb1f7a2c63b760462b485f9bb9f0&format=json`
+const getPhoto = ({farm, server, id, secret}) => `${getPhotoUrl(farm, server, id, secret)}`
 
 export function getPhotos(text, page) {
   return `${getFlickerAPI('flickr.photos.search')}&per_page=25&text=${text}&page=${page}`
 }
 
-export function getSmallPhoto({ farm, server, id, secret }) {
-  return `${getPhotoUrl(farm, server, id, secret)}_s.jpg`
+export function getSmallPhoto(photo) {
+  return `${getPhoto(photo)}_s.jpg`
+}
+
+export function getMediumPhoto(photo) {
+  return `${getPhoto(photo)}.jpg`
 }
 
 export function getPhotoInfo(id) {
