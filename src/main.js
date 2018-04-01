2660f45b8419057fdb19ad16ad84ad303d808d6c
Elvis Dedic <elvis26112009@gmail.com>
2018-04-01 11:09:59 +0200

    dont use content

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Content } from 'native-base'
+import { Container, Content, View } from 'native-base'
 import FlickerHeader from './components/FlickerHeader'
 import FlickerSearch from './components/FlickerSearch'
 import FlickerList from './components/FlickerList'
@@ -8,9 +8,9 @@ const MainComponent = () =>
   <Container>
     <FlickerHeader />
     <FlickerSearch />
-    <Content>      
+    <Container>      
       <FlickerList />
-    </Content>
+    </Container>
   </Container>
 
 export default MainComponent
